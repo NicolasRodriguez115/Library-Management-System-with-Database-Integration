00ea6e2e3cbe22baa41d0f71428ec8b5d70bea1f
NicolasRodriguez115 <nr115935@gmail.com>
2024-07-04 15:49:44 -0400

    you can now display all of the authors from the authors table in the db

--- app/author_operations/display_all_authors.py
+++ app/author_operations/display_all_authors.py
@@ -1,10 +1,25 @@
-from app.classes.author import authors
+from database.connect import connect_db
+from mysql.connector import Error
 def display_authors():
     print("""
 Authors:
 ------------
 """)
-    for  author in authors:
-        author.show_author()
-    input("When you're done checking the list of users press 'enter'.\n ")
-    return
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+
+        query = "SELECT * FROM authors"
+
+        cursor.execute(query)
+
+        for row in cursor.fetchall():
+            print(row) 
+
+    except Error as e:
+        print(f"Error: {e}")
+
+    finally:
+        if conn and conn.is_connected():
+            cursor.close()
+            conn.close()     
--- app/author_operations/menu.py
+++ app/author_operations/menu.py
@@ -1,6 +1,5 @@
 import os
 from app.author_operations.add_new_author import new_author
-from app.author_operations.view_author_details import author_details
 from app.author_operations.display_all_authors import display_authors
 def author_operations_menu():
     while True:
@@ -8,7 +7,6 @@ def author_operations_menu():
         user_input = input("""
 Author Operations:                         
     1. Add a new author
-    2. View author details
     3. Display all authors
     4. Quit
 
@@ -17,12 +15,9 @@ Author Operations:
             os.system("cls")
             new_author()
         elif user_input == "2":
-           os.system("cls")
-           author_details()
-        elif user_input == "3":
             os.system("cls")
             display_authors()
-        elif user_input == "4":
+        elif user_input == "3":
             return
         else:
             input("You've not entered a valid option. Please try again after pressing 'enter'\n ")
